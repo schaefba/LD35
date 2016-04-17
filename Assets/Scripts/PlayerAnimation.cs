ade4b5474b1ca94d418d1f3f96732913a4ed4b89
Tyler Manser <tyler.manser@gmail.com>
2016-04-17 15:18:30 -0600

    adding in animations fixing enemy prefab fixing level bugs

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimation : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(animator)
+		{
+
+			if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)) //left
+			{
+				animator.SetBool("player_walk", true);
+				animator.SetBool("player_idle", false);
+				
+			}
+			else
+            {
+                animator.SetBool("player_idle", true);
+                animator.SetBool("player_walk", false);
+            }
+		}
+	}
+}
