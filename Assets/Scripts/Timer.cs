6ab76f83f2c0b5ecc13ad5df99e842dcf66aba36
Chris Schaefbauer <schaefba@colorado.edu>
2016-04-16 18:22:40 -0600

    Add countdown timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public Text timerText;
+	public float timeForLevel;
+	public GameManager gameManager;
+
+	private float timeLeft;
+
+	void Awake () {
+
+		timeLeft = timeForLevel;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		timerText = gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Time.timeScale == 0) {
+			return;
+		}
+
+		timeLeft -= Time.deltaTime;
+
+		if (timeLeft <= 0) {
+			timerText.text = "0 Seconds";
+			GameManager.LevelFailed ();
+		} else {
+			timerText.text = System.String.Format ("{0} Seconds", Mathf.Round (timeLeft));
+		}
+	}
+}
