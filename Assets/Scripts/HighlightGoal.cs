9081c48a5383328230f45a0430110b0987bdf0c4
Chris Schaefbauer <schaefba@colorado.edu>
2016-04-18 17:42:21 -0600

    Adding flashing goal highlight for first level

---
+++ Assets/Scripts/HighlightGoal.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HighlightGoal : MonoBehaviour {
+
+	public Image goalHighlight;
+
+	void Awake() {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine (FlashGoalHighlight ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator FlashGoalHighlight() {
+	
+		for (int i = 0; i < 6; i++) {
+			
+			goalHighlight.enabled = true;
+			yield return new WaitForSeconds (.25f);
+			goalHighlight.enabled = false;
+			yield return new WaitForSeconds (.25f);
+		}
+
+	}
+}
